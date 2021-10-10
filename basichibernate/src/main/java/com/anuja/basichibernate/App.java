9df2b2a7d946fb3950092748c0e5043ef3425b1f
Anuja200 <bmanujaindia@gmail.com>
2021-10-10 14:13:39 +0530

    first commit

---
+++ basichibernate/src/main/java/com/anuja/basichibernate/App.java
@@ -0,0 +1,17 @@
+package com.anuja.basichibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	worker worker1 = new worker();
+    	worker1.setId(1);
+    	worker1.setName("Anuja");
+    	worker1.setDept("CSE");
+    }
+}
